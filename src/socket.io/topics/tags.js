239da76dc750d689ac077beaeeb586f3e75314bb
choksis8168 <shreya.choksi@yahoo.com>
2024-01-25 23:32:47 -0500

    Trying to get HW1 checks to pass

--- src/socket.io/topics/tags.js
+++ src/socket.io/topics/tags.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -37,7 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const meta_1 = __importDefault(require("../../meta"));
 const user_1 = __importDefault(require("../../user"));
-const topics = __importStar(require("../../topics"));
+const topics_1 = __importDefault(require("../../topics"));
 const categories_1 = __importDefault(require("../../categories"));
 const privileges_1 = __importDefault(require("../../privileges"));
 const utils_1 = __importDefault(require("../../utils"));
@@ -113,7 +90,7 @@ const SocketTopics = {
             data.cids = Cids;
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const result = yield topics.autocompleteTags(data);
+            const result = yield topics_1.default.autocompleteTags(data);
             return result.map(tag => tag.value);
         });
     },
@@ -121,7 +98,7 @@ const SocketTopics = {
         return __awaiter(this, void 0, void 0, function* () {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const result = yield searchTags(socket.uid, topics.searchTags, data);
+            const result = yield searchTags(socket.uid, topics_1.default.searchTags, data);
             return result.map(tag => tag.value);
         });
     },
@@ -129,7 +106,7 @@ const SocketTopics = {
         return __awaiter(this, void 0, void 0, function* () {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            return yield searchTags(socket.uid, topics.searchAndLoadTags, data);
+            return yield searchTags(socket.uid, topics_1.default.searchAndLoadTags, data);
         });
     },
     loadMoreTags(socket, data) {
@@ -146,7 +123,7 @@ const SocketTopics = {
             const cids = yield categories_1.default.getCidsByPrivilege('categories:cid', socket.uid, 'topics:read');
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            const tags = yield topics.getCategoryTagsData(cids, start, stop);
+            const tags = yield topics_1.default.getCategoryTagsData(cids, start, stop);
             return { tags: tags.filter(Boolean), nextStart: stop + 1 };
         });
     },
